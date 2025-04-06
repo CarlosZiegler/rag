df4d0cf0b1bd0eb483f3d8fc4072b0e50b6fc8c8
carlosziegler <carlos.ziegler@gmail.com>
2025-04-06 06:37:08 +0200

    feat: adjust build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    viewTransition: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
